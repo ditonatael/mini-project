bf5ccf8d4e4e0b73e184381cfafae6db7e3e8a9c
mac <mac@macs-MacBook-Pro.local>
2025-02-04 21:44:49 +0700

    feat: navbar and hero responsive layout

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,37 +3,37 @@ import { Button } from "../ui/button";
 
 export default function HeroSection() {
   return (
-    <div className="w-full flex">
-      <div className="w-1/2 h-[595px] relative overflow-hidden">
+    <div className="w-full flex flex-col lg:flex-row items-center justify-center group">
+      <div className="w-full lg:w-1/2 h-[270px] md:h-[400px] lg:h-[595px] relative overflow-hidden">
         <Image
           src={"/hero-menswear.webp"}
           alt="Menswear"
           fill={true}
           quality={100}
-          className="object-cover object-[25%_30%] hover:scale-105 transition-transform duration-700 hover:cursor-pointer"
+          className="object-cover object-[25%_30%] group-hover:scale-105 transition-transform duration-700 hover:cursor-pointer opacity-95"
         />
-        <div className="absolute h-[125px] w-full m-auto right-0 bottom-40 left-0 flex flex-col items-center justify-center gap-4">
-          <span className="text-5xl font-extrabold text-white drop-shadow-xl">
+        <div className="absolute h-[125px] w-full m-auto right-0 -bottom-3 md:bottom-7 lg:bottom-40 left-0 flex flex-col items-center justify-center gap-1 lg:gap-4">
+          <span className="text-2xl lg:text-5xl font-extrabold text-white drop-shadow-xl">
             Menswear
           </span>
-          <Button className="w-[126px] h-[45px] bg-white text-black text-lg font-bold hover:bg-white">
+          <Button className="w-[100px] md:w-[126px] h-[30px] md:h-[45px] bg-white text-black lg:text-lg font-bold hover:bg-white">
             Shop Now
           </Button>
         </div>
       </div>
-      <div className="w-1/2 h-[595px] relative overflow-hidden">
+      <div className="w-full lg:w-1/2 h-[270px] md:h-[400px] lg:h-[595px] relative overflow-hidden">
         <Image
           src={"/hero-womenswear.webp"}
           alt="Menswear"
           fill={true}
           quality={100}
-          className="object-cover hover:scale-105 transition-transform duration-700 hover:cursor-pointer"
+          className="object-cover group-hover:scale-105 transition-transform duration-700 hover:cursor-pointer"
         />
-        <div className="absolute h-[125px] w-full m-auto right-0 bottom-40 left-0 flex flex-col items-center justify-center gap-4">
-          <span className="text-5xl font-extrabold text-white drop-shadow-xl">
+        <div className="absolute h-[125px] w-full m-auto right-0 -bottom-3 md:bottom-7 lg:bottom-40 left-0 flex flex-col items-center justify-center gap-1 lg:gap-4">
+          <span className="text-2xl lg:text-5xl font-extrabold text-white drop-shadow-xl">
             Womenswear
           </span>
-          <Button className="w-[126px] h-[45px] bg-white text-black text-lg font-bold hover:bg-white">
+          <Button className="w-[100px] md:w-[126px] h-[30px] md:h-[45px] bg-white text-black lg:text-lg font-bold hover:bg-white">
             Shop Now
           </Button>
         </div>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,34 +1,49 @@
 import Image from "next/image";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-import { Heart, ShoppingBag, Search } from "lucide-react";
+import { Heart, ShoppingBag, Search, Menu } from "lucide-react";
 
 export default function Navbar() {
   return (
     <div>
       <div className="h-16 border-b">
-        <div className="mx-8 h-full flex items-center justify-between">
-          <Image
-            src={"/depop-logo.png"}
-            alt="logo"
-            width={90}
-            height={24}
-            quality={100}
-          />
-          <div className="w-[600px] h-10 rounded-full border-2 border-black bg-[#f3f3f3] flex items-center justify-start px-3 gap-1">
+        <div className="mx-2 lg:mx-8 h-full flex items-center justify-between">
+          <span className="flex items-center gap-4">
+            <Menu
+              color="#000000"
+              strokeWidth={1.75}
+              size={25}
+              className="flex lg:hidden"
+            />
+            <Image
+              src={"/depop-logo.png"}
+              alt="logo"
+              width={90}
+              height={24}
+              quality={100}
+            />
+          </span>
+          <div className="w-[600px] h-10 rounded-full border-2 border-black bg-[#f3f3f3] hidden lg:flex items-center justify-start px-3 gap-1">
             <Search color="#545454" strokeWidth={1.75} size={20} />
             <Input
               className="h-1/2 border-0 bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 px-0 rounded-none placeholder:text-zinc-950 placeholder:font-medium"
               placeholder="Search for items, brands, or styles..."
             />
           </div>
-          <span className="w-[406px] h-10 flex items-center gap-6">
-            <span className="flex items-center gap-6">
+          <span className="lg:w-[406px] h-10 flex items-center gap-4 lg:gap-6">
+            <span className="flex items-center gap-2 lg:gap-6">
               <Heart
                 color="#000000"
                 strokeWidth={1.75}
                 size={25}
-                className="hover:cursor-pointer"
+                className="hidden lg:block hover:cursor-pointer"
+              />
+              {/* Small Screen Search Button */}
+              <Search
+                color="#000000"
+                strokeWidth={1.75}
+                size={25}
+                className="block lg:hidden hover:cursor-pointer"
               />
               <span className="relative">
                 <ShoppingBag
@@ -37,12 +52,17 @@ export default function Navbar() {
                   size={25}
                   className="hover:cursor-pointer"
                 />
-                {/* <span className="absolute size-5 bg-red-600 text-white rounded-full flex items-center justify-center text-xs font-bold -top-1.5 -right-1.5">
-                1
-              </span> */}
+                <span className="absolute size-5 bg-red-600 text-white rounded-full flex items-center justify-center text-xs font-bold -top-1.5 -right-1.5">
+                  1
+                </span>
               </span>
             </span>
-            <span className="flex items-center gap-2">
+            {/* Small Screen Button */}
+            <Button className="flex lg:hidden items-center justify-center w-[116px] h-8 rounded-none font-bold text-lg">
+              Sign Up
+            </Button>
+            {/* Large Screen Button */}
+            <span className="hidden lg:flex items-center gap-2">
               <Button className="w-[116px] h-8 rounded-none font-bold text-lg">
                 Sell Now
               </Button>
@@ -62,7 +82,7 @@ export default function Navbar() {
           </span>
         </div>
       </div>
-      <div className="h-[53px] mx-8 flex items-center">
+      <div className="h-[53px] mx-8 hidden lg:flex items-center">
         <span className="p-4 hover:bg-black hover:cursor-pointer hover:text-white font-extrabold ">
           Menswear
         </span>
