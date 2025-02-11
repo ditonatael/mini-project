1f8f94dada73deef12546f582e283a20fbbb6eaf
mac <mac@macs-MacBook-Pro.local>
2025-02-11 19:14:59 +0700

    feat: Signup page image

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+
 export default function Signup() {
-  return <div className="h-screen">Signup Here</div>;
+  return (
+    <div className="h-screen w-full flex">
+      <div className="w-1/2 h-full relative">
+        <Image
+          src={"/signup-asset.webp"}
+          alt="Signup"
+          fill
+          quality={100}
+          objectFit="cover"
+        />
+      </div>
+    </div>
+  );
 }
