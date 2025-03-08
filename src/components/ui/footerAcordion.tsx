34e192ed3ca9e44eb8015e8540744427ddfc3f2a
mac <mac@macs-MacBook-Pro.local>
2025-03-08 15:04:32 +0700

    fix: image warnings, hydration error

--- src/components/ui/footerAcordion.tsx
+++ src/components/ui/footerAcordion.tsx
@@ -4,26 +4,43 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "../ui/accordion";
+import { useState, useEffect } from "react";
 
 export default function FooterAcordion({ footerItem }: { footerItem: any[] }) {
-  footerItem.push({
-    title: "Site Information",
-    children: ["Sitemaps", "Term and Conditions", "Privacy", "Cookies"],
-  });
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    footerItem.push({
+      title: "Site Information",
+      children: ["Sitemaps", "Term and Conditions", "Privacy", "Cookies"],
+    });
+
+    setIsClient(true);
+  }, []);
   return (
-    <div className="w-full block lg:hidden">
-      <Accordion type="single" collapsible className="w-full">
-        {footerItem.map((item, index) => {
-          return (
-            <AccordionItem value={item.title} key={index} className="w-full">
-              <AccordionTrigger>{item.title}</AccordionTrigger>
-              {item.children.map((child: string, index: number) => {
-                return <AccordionContent key={index}>{child}</AccordionContent>;
-              })}
-            </AccordionItem>
-          );
-        })}
-      </Accordion>
-    </div>
+    <>
+      {isClient ?? (
+        <div className="w-full block lg:hidden">
+          <Accordion type="single" collapsible className="w-full">
+            {footerItem.map((item, index) => {
+              return (
+                <AccordionItem
+                  value={item.title}
+                  key={index}
+                  className="w-full"
+                >
+                  <AccordionTrigger>{item.title}</AccordionTrigger>
+                  {item.children.map((child: string, index: number) => {
+                    return (
+                      <AccordionContent key={index}>{child}</AccordionContent>
+                    );
+                  })}
+                </AccordionItem>
+              );
+            })}
+          </Accordion>
+        </div>
+      )}
+    </>
   );
 }
--- src/components/ui/productCard.tsx
+++ src/components/ui/productCard.tsx
@@ -4,50 +4,51 @@ import Image from "next/image";
 import type { Products } from "../../../types/productType";
 import { useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function ProductCard({ products }: { products: Products }) {
   const [hovered, setHovered] = useState(false);
+  const router = useRouter();
   return (
     <div className="col-span-6 xl:col-span-3 w-[170px] h-[220px] md:w-[310px] md:h-[380px] flex flex-col items-center justify-center gap-4">
       <div
         className="w-full h-[90%] bg-zinc-100 relative rounded-md hover:cursor-pointer"
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
+        onClick={() => router.push(`/product/${products.id}`)}
       >
-        <Link href={`/product/${products.id}`}>
-          {/* Default Image */}
+        {/* Default Image */}
+        <Image
+          src={products.image[0]}
+          alt="Product Image"
+          fill
+          sizes="fill"
+          quality={100}
+          priority
+          className={`object-cover rounded-md transition-opacity duration-300 ${
+            hovered ? "opacity-0" : "opacity-100"
+          }`}
+        />
+        {/* Hovered Image */}
+        {products.image[1] && (
           <Image
-            src={products.image[0]}
-            alt="Product Image"
+            src={products.image[1]}
+            alt="Product Hover Image"
             fill
             sizes="fill"
             quality={100}
             loading="lazy"
-            className={`object-cover rounded-md transition-opacity duration-300 ${
-              hovered ? "opacity-0" : "opacity-100"
+            className={`object-cover rounded-md absolute top-0 left-0 transition-opacity duration-300 ${
+              hovered ? "opacity-100" : "opacity-0"
             }`}
           />
-          {/* Hovered Image */}
-          {products.image[1] && (
-            <Image
-              src={products.image[1]}
-              alt="Product Hover Image"
-              fill
-              sizes="fill"
-              quality={100}
-              loading="lazy"
-              className={`object-cover rounded-md absolute top-0 left-0 transition-opacity duration-300 ${
-                hovered ? "opacity-100" : "opacity-0"
-              }`}
-            />
-          )}
-          <Heart
-            color="#ffffff"
-            strokeWidth={1.75}
-            size={25}
-            className="hover:cursor-pointer absolute bottom-3 right-3"
-          />
-        </Link>
+        )}
+        <Heart
+          color="#ffffff"
+          strokeWidth={1.75}
+          size={25}
+          className="hover:cursor-pointer absolute bottom-3 right-3"
+        />
       </div>
       <div className="flex flex-col items-start justify-center w-full">
         <span className="text-lg font-bold">${products.price}</span>
