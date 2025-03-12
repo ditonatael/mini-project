78e64ba6663ccbc515c6e99440907d5ccfd263e0
mac <mac@macs-MacBook-Pro.local>
2025-03-12 12:06:51 +0700

    feat: logout

--- src/components/Forms/SignupForm/SignupFormSchema.ts
+++ src/components/Forms/SignupForm/SignupFormSchema.ts
@@ -2,21 +2,23 @@ import { z } from "zod"
 
 export const SignupFormSchema = z.object({
     firstname: z.string()
+        .nonempty({ message: "First name is required" })
         .min(2, { message: "First name must be at least 2 characters" })
-        .max(50, { message: "First name must be at most 50 characters" })
-        .nonempty({ message: "First name is required" }),
+        .max(50, { message: "First name must be at most 50 characters" }),
 
     lastname: z.string()
+        .nonempty({ message: "Last name is required" })
         .min(2, { message: "Last name must be at least 2 characters" })
-        .max(50, { message: "Last name must be at most 50 characters" })
-        .nonempty({ message: "Last name is required" }),
+        .max(50, { message: "Last name must be at most 50 characters" }),
 
     email: z.string()
         .nonempty({ message: "Email is required" })
         .email({ message: "Invalid email format" }),
 
     username: z.string()
-        .nonempty({ message: "Username is required" }),
+        .nonempty({ message: "Username is required" })
+        .min(2, { message: "Username must be at least 2 characters" })
+        .max(10, { message: "Username must be at most 10 characters" }),
 
     password: z.string()
         .min(5, { message: "Password must be at least 5 characters long" })
--- src/components/ui/userAvatar.tsx
+++ src/components/ui/userAvatar.tsx
@@ -1,3 +1,17 @@
+"use client";
+
+import { ChevronDown, LogOut } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import { useContext } from "react";
+import { userContext } from "~/supports/context/useUserContext";
+
 export default function UserAvatar({
   name,
   className,
@@ -5,12 +19,41 @@ export default function UserAvatar({
   name: string;
   className: string;
 }) {
+  const { setUserData } = useContext(userContext);
+
+  const handleLogout = () => {
+    setUserData(null);
+    localStorage.clear();
+  };
   return (
     <div
-      className={`size-10 bg-stone-200 rounded-full flex items-center justify-center text-sm font-bold ${className}`}
+      className={`h-8 bg-red-600 flex items-center justify-start gap-2 rounded-full min-w-24 w-fit px-3 ${className}`}
     >
-      {" "}
-      {name.charAt(0)}
+      <div className="size-6 rounded-full bg-white flex items-center justify-center text-sm font-semibold">
+        {name.charAt(0)}
+      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <div className="text-white line-clamp-1 text-sm flex items-center gap-4">
+            {name} <ChevronDown size={15} />
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            className="flex items-center text-red-600"
+            onClick={() => handleLogout()}
+          >
+            <LogOut /> Logout
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
