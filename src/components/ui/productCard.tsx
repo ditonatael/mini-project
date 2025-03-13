79b76c512aba4916d73535253e54de427381da05
mac <mac@macs-MacBook-Pro.local>
2025-03-13 13:10:58 +0700

    fix: remove unused  link

--- src/components/ui/productCard.tsx
+++ src/components/ui/productCard.tsx
@@ -3,7 +3,6 @@ import { Heart } from "lucide-react";
 import Image from "next/image";
 import type { Products } from "../../../types/productType";
 import { useState } from "react";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function ProductCard({ products }: { products: Products }) {
